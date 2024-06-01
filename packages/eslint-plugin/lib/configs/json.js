2c76a67c0514ce29f5e39aef466f28288d266a10
Brion <info@brionmario.com>
2024-06-01 17:30:45 +0530

    fear(eslint): add `json` config

---
+++ packages/eslint-plugin/lib/configs/json.js
@@ -0,0 +1,86 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2024, Brion Mario
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ * @fileoverview ESLint config to be used to lint JSON files.
+ */
+
+module.exports = {
+  overrides: [
+    {
+      files: 'package.json',
+      parser: 'json-eslint-parser',
+      pathPattern: '^$',
+      rules: {
+        'header/header': 'off',
+        'jsonc/sort-array-values': [
+          'error',
+          {
+            order: [
+              'private',
+              'name',
+              'version',
+              'description',
+              'author',
+              'license',
+              'sideEffects',
+              'type',
+              'files',
+              'main',
+              'module',
+              'types',
+              'exports',
+              'homepage',
+              'bugs',
+              'repository',
+              'keywords',
+              'scripts',
+              'dependencies',
+              'devDependencies',
+              'peerDependencies',
+              'peerDependenciesMeta',
+              'publishConfig',
+              'browserslist',
+              'engines',
+              'workspaces',
+            ],
+            pathPattern: '^$',
+          },
+        ],
+        'jsonc/sort-keys': 'error',
+        'max-len': 'off',
+        semi: 'off',
+      },
+    },
+    {
+      files: '*.json',
+      parser: 'jsonc-eslint-parser',
+      rules: {
+        'header/header': 'off',
+        'max-len': 'off',
+        semi: 'off',
+      },
+    },
+  ],
+};
